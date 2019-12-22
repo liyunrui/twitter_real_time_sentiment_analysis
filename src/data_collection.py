0e8cddc599a955b10baeabbd9237a614481a20b4
李昀叡 <yunruili@liyunruide-MacBook-Pro.local>
2019-12-22 17:13:49 +0800

    final

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,3 +1,9 @@
+"""
+Get the latstes real_time tweet data from twitter
+
+python3 data_collection.py --url https://twitter.com/realDonaldTrump --real_time True
+
+"""
 import pandas as pd
 import argparse
 import os
@@ -19,24 +25,47 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--url', default='https://twitter.com/realDonaldTrump', type=str, required = True)
     parser.add_argument('--data_path', default="../dataset/raw_data", type=str)
+    parser.add_argument('--real_time', default=False, type=bool)
+    parser.add_argument('--num_pages', default=1, type=int)
+
     opt = parser.parse_args()
+    if opt.real_time == False:
+        twitter_url = opt.url
+        user_name = get_username(twitter_url)
+        list_of_tweets = get_tweets(user_name, pages=50000)
+        columns = [
+         'tweetId',
+         'isRetweet',
+         'time',
+         'text',
+         'replies',
+         'retweets',
+         'likes',
+         'entries']
+        data = {c:[] for c in columns}
+        for tweet in list_of_tweets:
+            for col in columns:
+                data[col].append(tweet[col])
 
-    twitter_url = opt.url
-    user_name = get_username(twitter_url)
-    list_of_tweets = get_tweets(user_name, pages=50000)
-    columns = [
-     'tweetId',
-     'isRetweet',
-     'time',
-     'text',
-     'replies',
-     'retweets',
-     'likes',
-     'entries']
-    data = {c:[] for c in columns}
-    for tweet in list_of_tweets:
-        for col in columns:
-            data[col].append(tweet[col])
+        df = pd.DataFrame(data)
+        df.to_csv(os.path.join(opt.data_path, "{}.csv".format(user_name)), index = False)
+    else:
+        twitter_url = opt.url
+        user_name = get_username(twitter_url)
+        list_of_tweets = get_tweets(user_name, pages=opt.num_pages)
+        columns = [
+         'tweetId',
+         'isRetweet',
+         'time',
+         'text',
+         'replies',
+         'retweets',
+         'likes',
+         'entries']
+        data = {c:[] for c in columns}
+        for tweet in list_of_tweets:
+            for col in columns:
+                data[col].append(tweet[col])
 
-    df = pd.DataFrame(data)
-    df.to_csv(os.path.join(opt.data_path, "{}.csv".format(user_name)), index = False)
+        df = pd.DataFrame(data)
+        df.to_csv(os.path.join(opt.data_path, "{}_real_time.csv".format(user_name)), index = False)
--- src/data_tagging.py
+++ src/data_tagging.py
@@ -1,3 +1,7 @@
+"""
+python3 data_tagging.py --input_path ../dataset/raw_data/realDonaldTrump_real_time.csv --real_time True --output_path ../output/
+
+"""
 import os
 import argparse
 import pandas as pd
@@ -19,64 +23,80 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_path', default="../dataset/raw_data", type=str)
     parser.add_argument('--output_path', default="../dataset/tagged_data", type=str)
+    parser.add_argument('--real_time', default=False, type=bool)
 
     opt = parser.parse_args()
 
     data_dir = opt.input_path
-    out = []
-    for file_name in os.listdir(data_dir):
-        user_name = file_name[:-4]
-        file_path = os.path.join(data_dir,file_name)
-        df = pd.read_csv(file_path)
-        df["user_name"] = [user_name for i in range(len(df))]
-        out.append(df)
-        #print (user_name)
-        #print (file_path)
-    df = pd.concat(out, axis = 0)
-    print ("num of raw data we crawled for model training", len(df))
+    if opt.real_time == False:
+        out = []
+        for file_name in os.listdir(data_dir):
+            user_name = file_name[:-4]
+            file_path = os.path.join(data_dir,file_name)
+            df = pd.read_csv(file_path)
+            df["user_name"] = [user_name for i in range(len(df))]
+            out.append(df)
+            #print (user_name)
+            #print (file_path)
+        df = pd.concat(out, axis = 0)
+        print ("num of raw data we crawled for model training", len(df))
 
-    #------------------------
-    # data processing
-    #------------------------
-    # parse time
-    df = time_parser(df)
-    # text cleaning
-    df = tweets_cleaning(df,"text")
+        #------------------------
+        # data processing
+        #------------------------
+        # parse time
+        df = time_parser(df)
+        # text cleaning
+        df = tweets_cleaning(df,"text")
 
-    #------------------------
-    # data tagging
-    #------------------------
+        #------------------------
+        # data tagging
+        #------------------------
 
-    #positive words
-    positive_words = pd.read_csv("../asset/positive-words.txt",
-                                 header=None)
-    #negative words
-    negative_words = pd.read_csv("../asset/negative-words.txt",
-                                 header=None,encoding='latin-1')
+        #positive words
+        positive_words = pd.read_csv("../asset/positive-words.txt",
+                                     header=None)
+        #negative words
+        negative_words = pd.read_csv("../asset/negative-words.txt",
+                                     header=None,encoding='latin-1')
 
-    positive_words_list = convert_words_list(positive_words)
+        positive_words_list = convert_words_list(positive_words)
 
-    #remove word trump from positive word list
-    positive_words_list = [i for i in positive_words_list if i not in "trump"]
-    negative_words_list = convert_words_list(negative_words)
-    df = scoring_tweets(df,"text",positive_words_list,negative_words_list)
-    print ("Tagging Finished !")
-    # save
-    check_dir(opt.output_path)
-    df.to_csv(os.path.join(opt.output_path, "{}.csv".format("tagging")), index = False)
-    #------------------------
-    # exp
-    #------------------------
-    df = pd.read_csv(os.path.join(opt.output_path, "{}.csv".format("tagging")))
-    df.dropna(subset = ["text"], inplace = True)
-    print ("num_data : ", len(df))
-    df_train = df.sample(frac = 0.8)
-    print ("num_training_data : ", len(df_train))
-    df_test = df[~df.index.isin(df_train.index)]
-    print ("num_testing_data : ", len(df_test))
-    assert len(df_train)+len(df_test)==len(df), "it should be the same."
+        #remove word trump from positive word list
+        positive_words_list = [i for i in positive_words_list if i not in "trump"]
+        negative_words_list = convert_words_list(negative_words)
+        df = scoring_tweets(df,"text",positive_words_list,negative_words_list)
+        print ("Tagging Finished !")
+        # save
+        check_dir(opt.output_path)
+        df.to_csv(os.path.join(opt.output_path, "{}.csv".format("tagging")), index = False)
+        #------------------------
+        # exp
+        #------------------------
+        df = pd.read_csv(os.path.join(opt.output_path, "{}.csv".format("tagging")))
+        df.dropna(subset = ["text"], inplace = True)
+        print ("num_data : ", len(df))
+        df_train = df.sample(frac = 0.8)
+        print ("num_training_data : ", len(df_train))
+        df_test = df[~df.index.isin(df_train.index)]
+        print ("num_testing_data : ", len(df_test))
+        assert len(df_train)+len(df_test)==len(df), "it should be the same."
 
-    to_txt(df_train,dataset_files["twitter"]["train"])
-    to_txt(df_test,dataset_files["twitter"]["test"])
+        to_txt(df_train,dataset_files["twitter"]["train"])
+        to_txt(df_test,dataset_files["twitter"]["test"])
+    else:
+        #------------------------
+        # data processing
+        #------------------------
+        user_name = data_dir.split("/")[-1][:-14]
+        df = pd.read_csv(data_dir)
+        # parse time
+        df = time_parser(df)
+        # text cleaning
+        df = tweets_cleaning(df,"text")
+        print ("Data Processing Finished !")
+        # save
+        check_dir(opt.output_path)
+        df.to_csv(os.path.join(opt.output_path, "{}.csv".format("{}_processed_data".format(user_name))), index = False)
 
 
